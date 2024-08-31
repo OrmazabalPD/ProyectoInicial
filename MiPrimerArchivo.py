faeb11d15d8fee2f1e4dfe72831db33d3f34e245
Domingo Ormazabal <ormazabal.domingo@gmail.com>
2024-08-31 18:03:16 -0400

    este es mi tercer commit calculadora terminada

--- MiPrimerArchivo.py
+++ MiPrimerArchivo.py
@@ -18,30 +18,36 @@ def multiplicar(a,b):
 def dividir(a,b):
     return a/b
 
-print("Bienvenido a la calculadora")
-print("1. Sumar")
-print("2. Restar")
-print("3. Multiplicar")
-print("4. Dividir")
-print("5. Salir")
+while True:
+    print("Bienvenido a la calculadora")
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("5. Salir")
 
-eleccion = int(input("elija una opción: "))
-num1 = int(input("Ingrese el primer número: ")) 
-num2 = int(input("Ingrese el segundo número: "))
+    eleccion = int(input("elija una opción: "))
+    
 
-if eleccion == 1:
-    print(num1, "+",num2, "=", sumar(num1,num2))
+    if eleccion == 5:
+        print("Saliendo de la calculadora...")
+        break
 
-elif eleccion == 2:
-    print(num1, "-",num2, "=", restar(num1,num2))   
+    if eleccion < 1 or eleccion > 5:
+        print("Opción no válida, por favor elija una opción entre 1 y 5.")
+        continue
+    
+    num1 = int(input("Ingrese el primer número: ")) 
+    num2 = int(input("Ingrese el segundo número: "))
+    
+    if eleccion == 1:
+        print(num1, "+",num2, "=", sumar(num1,num2))
 
-elif eleccion == 3:
-    print(num1, "*",num2, "=", multiplicar(num1,num2))
+    elif eleccion == 2:
+        print(num1, "-",num2, "=", restar(num1,num2))   
 
-elif eleccion == 4:
-    print(num1, "/",num2, "=", dividir(num1,num2))
+    elif eleccion == 3:
+        print(num1, "*",num2, "=", multiplicar(num1,num2))
 
-else:
-    print("Opción no válida")
- # dfasasdfads
- 
+    elif eleccion == 4:
+        print(num1, "/",num2, "=", dividir(num1,num2))
